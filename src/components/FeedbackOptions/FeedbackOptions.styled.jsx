34ab81dac9dbdb21be84ca63d96cc4ab32d8031e
Roman <sayga76239@gmail.com>
2023-02-03 14:44:35 +0100

    style

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  padding: 5px 15px;
-  border-radius: 4px;
+  padding: 15px 20px;
+  border-radius: 15px;
   box-shadow: 4px 4px 4px rgb(0 0 0 / 15%);
   text-transform: capitalize;
   font-weight: bold;
